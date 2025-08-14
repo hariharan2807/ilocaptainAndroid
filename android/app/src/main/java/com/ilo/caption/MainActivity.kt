e3e14c67e7241e36a4cb797f0cb9f65976d77c0f
hari <hariharan12.gtech@gmail.com>
2025-08-14 16:58:11 +0530

    App Name Only change iLo Captain

--- android/app/src/main/java/com/ilo/caption/MainActivity.kt
+++ android/app/src/main/java/com/ilo/caption/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ILO Caption"
+  override fun getMainComponentName(): String = "iLo Captain"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/iLoCaptainTests/iLoCaptainTests.m
+++ ios/iLoCaptainTests/iLoCaptainTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ILOCaptionTests : XCTestCase
+@interface iLoCaptainTests : XCTestCase
 
 @end
 
-@implementation ILOCaptionTests
+@implementation iLoCaptainTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
